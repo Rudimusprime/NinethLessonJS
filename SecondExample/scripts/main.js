3c550be136b67890f7fb6b59470072d9470d002c
DanielB <d.banasiewicz@pdo.com.pl>
2021-09-26 18:11:27 +0200

    Second example done

--- SecondExample/scripts/main.js
+++ SecondExample/scripts/main.js
@@ -11,7 +11,7 @@ const slideList = [{
 const image = document.querySelector('img.slider')
 const h1 = document.querySelector('h1.slider')
 
-const time = 1000;
+const time = 5000;
 let active = 0;
 const dots = [...document.querySelectorAll('.dots span')];
 
@@ -30,4 +30,24 @@ const changeSlide = () => {
     h1.textContent = slideList[active].text;
     changeDot();
 }
-setInterval(changeSlide, time);
+let indexInterval = setInterval(changeSlide, time);
+
+const keyChangeSlide = (e) => {
+    if (e.keyCode === 37 || e.keyCode === 39) {
+        clearInterval(indexInterval);
+        e.keyCode === 37 ? active-- : active++;
+        if (active === slideList.length) {
+            active = 0;
+        } else if (active < 0) {
+            active = slideList.length - 1;
+        }
+        image.src = slideList[active].img;
+        h1.textContent = slideList[active].text;
+        changeDot();
+        console.log(active);
+        indexInterval = setInterval(changeSlide, time);
+    }
+
+}
+
+window.addEventListener('keydown', keyChangeSlide);
